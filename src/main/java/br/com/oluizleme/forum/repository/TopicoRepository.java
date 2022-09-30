3223eea6b6c4c3c7ad5f2561b70266115a3f5ceb
oluizleme <o.luizleme@gmail.com>
2022-09-29 21:19:14 -0300

    refactor(main): Implementado o método para consultar os tópicos pelo nome do curso

--- src/main/java/br/com/oluizleme/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/oluizleme/forum/repository/TopicoRepository.java
@@ -3,5 +3,8 @@ package br.com.oluizleme.forum.repository;
 import br.com.oluizleme.forum.modelo.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    List<Topico> findByCursoNome(String nomeCurso);
 }
