f8511bc08115ec98e08dcac4847e589f274de35c
oluizleme <o.luizleme@gmail.com>
2022-09-29 21:00:38 -0300

    chore(main): Anotada a classe com as anotações de JPA e configurações de relacionamentos

--- src/main/java/br/com/oluizleme/forum/controller/TopicosController.java
+++ src/main/java/br/com/oluizleme/forum/controller/TopicosController.java
@@ -3,6 +3,8 @@ package br.com.oluizleme.forum.controller;
 import br.com.oluizleme.forum.controller.dto.TopicoDTO;
 import br.com.oluizleme.forum.modelo.Curso;
 import br.com.oluizleme.forum.modelo.Topico;
+import br.com.oluizleme.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,12 @@ import java.util.List;
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     public List<TopicoDTO> lista(){
-        Topico topico = new Topico("Dúvida Curso Spring Rest API", "Dúvida com Spring", new Curso("Spring", "Programação"));
-        return TopicoDTO.converter(List.of(topico));
+       List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDTO.converter(topicos);
     }
 }
