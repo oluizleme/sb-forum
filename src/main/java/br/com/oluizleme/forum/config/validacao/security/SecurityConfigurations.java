dd63596bd715c4dc713cb501a9c7171e6be67f4d
oluizleme <o.luizleme@gmail.com>
2023-02-14 07:26:21 -0300

    feat(main): Implementada as configurações de autenticacao da aplicação

--- src/main/java/br/com/oluizleme/forum/config/validacao/security/SecurityConfigurations.java
+++ src/main/java/br/com/oluizleme/forum/config/validacao/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.oluizleme.forum.config.validacao.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,10 +8,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AutenticacaoService autenticacaoService;
 
     //Configuracoes de Autorizacao
     @Override
@@ -30,5 +35,6 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     //Configuracoes de autenticacao
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
